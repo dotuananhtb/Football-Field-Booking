96099b7635e3f8ee91446d1c570b263c6ffa354e
Do Tuan Anh <dotuananhofficial@gmail.com>
2025-05-25 00:55:45 +0700

    a

---
+++ FB_N1/src/java/util/DBContext.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Đỗ Tuấn Anh
+ */
+public class DBContext {
+
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+// Edit URL , username, password to authenticate with your MS SQL Server
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=FootballFieldBooking";
+            String username = "sa";
+            String password = "123";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        DBContext db = new DBContext();
+        if (db.connection != null) {
+            System.out.println("✅ Kết nối đến cơ sở dữ liệu thành công!");
+        } else {
+            System.out.println("❌ Kết nối thất bại!");
+        }
+    }
+
+}
