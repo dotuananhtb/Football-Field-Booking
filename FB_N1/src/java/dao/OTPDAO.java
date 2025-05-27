e3f7671c82e25a6235be56d321abbeab1c439443
Do Tuan Anh <dotuananhofficial@gmail.com>
2025-05-27 11:34:12 +0700

    1

--- FB_N1/src/java/dao/OTPDAO.java
+++ FB_N1/src/java/dao/OTPDAO.java
@@ -13,7 +13,7 @@ public class OTPDAO {
 
     
 
-    // CREATE
+    /// CREATE
     public boolean insertOTP(OTP otp) {
         String sql = "INSERT INTO OTP (account_id, otp, otp_date) VALUES (?, ?, ?)";
         try (PreparedStatement stm = connection.prepareStatement(sql)) {
