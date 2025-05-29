af8894bb246f023a1217b68f02eac091e62f70a7
Do Tuan Anh <dotuananhofficial@gmail.com>
2025-05-30 03:19:01 +0700

    ok_path

--- FB_N1/src/java/controller/DangKi.java
+++ FB_N1/src/java/controller/DangKi.java
@@ -72,6 +72,8 @@ public class DangKi extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        request.getRequestDispatcher("UI/dangki.jsp").forward(request, response);
+
     }
 
     @Override
