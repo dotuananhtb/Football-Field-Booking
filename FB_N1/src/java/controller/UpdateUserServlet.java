77b3783a0f16a98aaf144e9ce151cb1c582a2593
tranducthanh04 <tranducthanh31102004@gmail.com>
2025-05-29 10:01:34 +0700

    fix updateprofile va tach header

--- FB_N1/src/java/controller/UpdateUserServlet.java
+++ FB_N1/src/java/controller/UpdateUserServlet.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import java.io.IOException;
@@ -14,40 +13,44 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import dao.UserProfileDAO;
 import model.*;
+
 /**
  *
  * @author Asus
  */
-@WebServlet(name="UpdateUserServlet", urlPatterns={"/updateUser"})
+@WebServlet(name = "UpdateUserServlet", urlPatterns = {"/updateUser"})
 public class UpdateUserServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet UpdateUserServlet</title>");  
+            out.println("<title>Servlet UpdateUserServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet UpdateUserServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet UpdateUserServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -55,12 +58,13 @@ public class UpdateUserServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -68,7 +72,7 @@ public class UpdateUserServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         String name = request.getParameter("name");
@@ -78,15 +82,20 @@ public class UpdateUserServlet extends HttpServlet {
         String address = request.getParameter("address");
         String id = request.getParameter("id");
         UserProfile u = new UserProfile(name, null, address, null, dob, phone, null);
+    
         UserProfileDAO dao = new UserProfileDAO();
         dao.updateProfile1(u, id);
         request.setAttribute("mess", "Update Successfully!");
         out.print(id);
-//        request.getRequestDispatcher("UI/userProfile.jsp").forward(request, response);
+        request.getRequestDispatcher("UI/userProfile.jsp").forward(request, response);
+
+//        UserProfile u = new UserProfile("aaa", null, "aaa", null, "2000-02-02", "0123", null);
+//        dao.updateProfile1(u, "4");
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
