2941ff1b6401dc4105634787b1e36728882e6aa3
Do Tuan Anh <dotuananhofficial@gmail.com>
2025-06-10 11:46:33 +0700

    a

--- FB_N1/src/java/controller/HomeControl.java
+++ FB_N1/src/java/controller/HomeControl.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dao.*;
@@ -27,7 +26,7 @@ import model.*;
  *
  * @author VAN NGUYEN
  */
-@WebServlet(name = "HomeControl", urlPatterns = { "/home" })
+@WebServlet(name = "HomeControl", urlPatterns = {"/home"})
 
 public class HomeControl extends HttpServlet {
 
@@ -35,11 +34,11 @@ public class HomeControl extends HttpServlet {
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
-     * 
-     * @param request  servlet request
+     *
+     * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException      if an I/O error occurs
+     * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -53,27 +52,24 @@ public class HomeControl extends HttpServlet {
         request.setAttribute("listF", list);
         request.setAttribute("listZ", listZ);
 
-    
+        FieldDAO fdao = new FieldDAO();
+        ProductDAO pdao = new ProductDAO();
+        Vector<Product> plist = pdao.getAllProductsWithCategory();
+        request.setAttribute("plist", plist);
 
-    
-    FieldDAO fdao = new FieldDAO();
-    Vector<Field> list = new Vector<>();
-    ProductDAO pdao = new ProductDAO();
-    Vector<Product> plist = pdao.getAllProductsWithCategory();request.setAttribute("plist",plist);
+        Vector<FieldDetails> fieldList = fdao.getAllFieldDetails();
+        request.setAttribute("fieldList", fieldList);
 
-    Vector<FieldDetails> fieldList = fdao.getAllFieldDetails();request.setAttribute("fieldList",fieldList);
-    UserProfileDAO dao = new UserProfileDAO();
-    Contact ct = dao.getContact();request.setAttribute("ct",ct);
+        request.getRequestDispatcher("UI/trang-chu.jsp").forward(request, response);
 
-    request.getRequestDispatcher("UI/trang-chu.jsp").forward(request,response);
-
-    // response.sendRedirect("UI/trang-chu.jsp");
+        // response.sendRedirect("UI/trang-chu.jsp");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
     // + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -81,7 +77,6 @@ public class HomeControl extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-
             throws ServletException, IOException {
 
         request.getRequestDispatcher("UI/homePage.jsp").forward(request, response);
@@ -89,22 +84,21 @@ public class HomeControl extends HttpServlet {
 
     /**
      * Handles the HTTP <code>POST</code> method.
-     * 
-     * @param request  servlet request
+     *
+     * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException      if an I/O error occurs
+     * @throws IOException if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
     /**
      * Returns a short description of the servlet.
-     * 
+     *
      * @return a String containing servlet description
      */
     @Override
