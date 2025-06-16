2e7d6c8c668e43e929c39e8848a32f9275911824
tranducthanh04 <tranducthanh31102004@gmail.com>
2025-06-16 21:21:04 +0700

    up

--- FB_N1/src/java/controller/ManagerPostServlet.java
+++ FB_N1/src/java/controller/ManagerPostServlet.java
@@ -1,8 +1,8 @@
 package controller;
 
 import dao.PostDAO;
+import dao.CommentDAO;
 import model.Post;
-import model.Account;
 import java.io.IOException;
 import java.util.List;
 import jakarta.servlet.ServletException;
@@ -10,29 +10,20 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 @WebServlet(name = "ManagerPostServlet", urlPatterns = {"/admin/managerPost"})
 public class ManagerPostServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-//        Account acc = (Account) session.getAttribute(   "account");
-//        if (acc == null || (acc.getRoleId() != 1 && acc.getRoleId() != 2)) {
-//            response.sendRedirect(request.getContextPath() + "/home");
-//            return;
-//        }
         PostDAO postDAO = new PostDAO();
+        CommentDAO commentDAO = new CommentDAO();
         List<Post> posts = postDAO.getAllPosts();
+        for (Post post : posts) {
+            int count = commentDAO.countCommentsByPostId(post.getPostId());
+            post.setCommentCount(count);
+        }
         request.setAttribute("posts", posts);
-        request.setAttribute("pageTitle", "Quản lý bài viết");
-        request.setAttribute("colId", "ID");
-        request.setAttribute("colTitle", "Tiêu đề");
-        request.setAttribute("colAuthor", "Tác giả");
-        request.setAttribute("colDate", "Ngày đăng");
-        request.setAttribute("colStatus", "Trạng thái");
-        request.setAttribute("colAction", "Hành động");
         request.getRequestDispatcher("/admin/ManagerPost.jsp").forward(request, response);
     }
 } 
