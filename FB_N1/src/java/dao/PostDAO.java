f52a8dfc452e2da1a6e8a99cdc029bdcf91f4a07
tranducthanh04 <tranducthanh31102004@gmail.com>
2025-06-17 22:15:49 +0700

    up

--- FB_N1/src/java/dao/PostDAO.java
+++ FB_N1/src/java/dao/PostDAO.java
@@ -60,9 +60,7 @@ public class PostDAO extends DBContext {
         }
         return listPost;
     }
-<<<<<<< HEAD
 
-=======
      public Vector<Post> get3LastestPost() {
         String sql = "SELECT TOP 3 *\n"
                 + "FROM Post\n"
@@ -83,7 +81,7 @@ public class PostDAO extends DBContext {
         }
         return listPost;
     }
->>>>>>> origin/anh_he180507
+
     public Vector<Post> getAllPosts(int page, int pageSize) {
         Vector<Post> list = new Vector<>();
         String query = "SELECT p.*, a.username FROM Post p " +
@@ -272,26 +270,7 @@ public class PostDAO extends DBContext {
             e.printStackTrace();
         }
     }
-public Vector<Post> get3LastestPost() {
-        String sql = "SELECT TOP 3 *\n"
-                + "FROM Post\n"
-                + "WHERE status_post = 'active'\n"
-                + "ORDER BY post_date DESC";
-        Vector<Post> listPost = new Vector<>();
-        try {
-            PreparedStatement ptm = connection.prepareStatement(sql);
-            ResultSet rs = ptm.executeQuery();
-            while (rs.next()) {
-                Post p = new Post(rs.getInt(1), rs.getInt(2),
-                        rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6)
-                );
-                listPost.add(p);
-            }
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return listPost;
-    }
+
     public void createPost(Post post) {
         String sql = "INSERT INTO Post (account_id, title, content_post, post_date, status_post) VALUES (?, ?, ?, GETDATE(), ?)";
         try {
